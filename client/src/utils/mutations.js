10c3b7c7e6cfd57e6074aee272cdc7e85c7767a8
Matt Tomchik <matthew.tomchik@yahoo.com>
2023-02-22 20:21:17 -0700

    changed api to mutations and updated

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,57 +1,23 @@
-// route to get logged in user's info (needs the token)
-export const getMe = (token) => {
-  return fetch('/api/users/me', {
-    headers: {
-      'Content-Type': 'application/json',
-      authorization: `Bearer ${token}`,
-    },
-  });
-};
+import { gql } from '@apollo/client';
 
-export const createUser = (userData) => {
-  return fetch('/api/users', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(userData),
-  });
-};
+export const CREATE_MATCHUP = gql`
+  mutation createMatchup($tech1: String!, $tech2: String!) {
+    createMatchup(tech1: $tech1, tech2: $tech2) {
+      _id
+      tech1
+      tech2
+    }
+  }
+`;
 
-export const loginUser = (userData) => {
-  return fetch('/api/users/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(userData),
-  });
-};
-
-// save book data for a logged in user
-export const saveBook = (bookData, token) => {
-  return fetch('/api/users', {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-      authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify(bookData),
-  });
-};
-
-// remove saved book data for a logged in user
-export const deleteBook = (bookId, token) => {
-  return fetch(`/api/users/books/${bookId}`, {
-    method: 'DELETE',
-    headers: {
-      authorization: `Bearer ${token}`,
-    },
-  });
-};
-
-// make a search to google books api
-// https://www.googleapis.com/books/v1/volumes?q=harry+potter
-export const searchGoogleBooks = (query) => {
-  return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
-};
+export const CREATE_VOTE = gql`
+  mutation createVote($_id: String!, $techNum: Int!) {
+    createVote(_id: $_id, techNum: $techNum) {
+      _id
+      tech1
+      tech2
+      tech1_votes
+      tech2_votes
+    }
+  }
+`;
