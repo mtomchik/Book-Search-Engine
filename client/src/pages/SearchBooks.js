9a10726808723665edf0e2cd0ceaa2f9b355a4e4
Matt Tomchik <matthew.tomchik@yahoo.com>
2023-02-22 20:30:33 -0700

    updated search

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -9,9 +9,10 @@ import {
   CardColumns,
 } from "react-bootstrap";
 
-import Auth from "../utils/auth";
-import { saveBook, searchGoogleBooks } from "../utils/mu";
-import { saveBookIds, getSavedBookIds } from "../utils/localStorage";
+import Auth from '../utils/auth';
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
+
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -37,7 +38,9 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await searchGoogleBooks(searchInput);
+      const response = await fetch(
+        `https://www.googleapis.com/books/v1/volumes?q=${searchInput}`
+      );
 
       if (!response.ok) {
         throw new Error("something went wrong!");
@@ -73,11 +76,9 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error("something went wrong!");
-      }
+      const { data } = await saveBook({
+        variables: { newBook: { ...bookToSave } },
+      });
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
